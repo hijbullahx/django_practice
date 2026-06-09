e8946b6fa4505a6c143789354e08aaaeafeb4a41
MD TAHER BIN OMAR HIJBULLAH <22303142@iubat.edu>
2026-06-09 08:31:07 +0000

    practice_tns_Day_01

---
+++ tns/Python_Practice.py
@@ -0,0 +1,35 @@
+#for i in range(10):
+#    print("Hijbullah")
+
+#for i in range(11):
+#    print(i)    
+
+#for i in range(0,101,10):
+#    print(i)
+
+#for i in range (5, 100, 5):
+   # if i == 90:
+    # continue
+   # print(i)
+
+#name = "Hamid"
+
+#for ch in name:
+ #   print(ch)
+
+#color = ["Red", "Green", "Blue"]
+#fruit = ["mango", 'banana', 'jackfruit']
+#for i in color:
+  #  for j in fruit:
+  #      print(i,j)
+
+#num = 7
+#for i in range(1,11):
+  #  print(f"{num} x {i} = {num*i}")
+
+
+#count = 0
+#while count<100:
+  #print(count)
+  #if count == 20:
+    #count+=5
