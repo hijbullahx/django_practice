59029cdc1a4c4ab977033df5309c365541e24613
MD TAHER BIN OMAR HIJBULLAH <22303142@iubat.edu>
2026-07-08 09:26:00 +0000

    add StudentAdmin to admin panel and implement student addition view

--- scnd_prjct/config/st_info/views.py
+++ scnd_prjct/config/st_info/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from st_info.models import Student
 
 # Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def add_student(request):
+    if request.method == "POST":
+        st_id = request.POST.get('st_id')
+        st_name = request.POST.get('st_name')
+        dept = request.POST.get('dept')
+        dob = request.POST.get('dob')
+        gender = request.POST.get('gender')
+        address = request.POST.get('address')
