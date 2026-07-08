6666add117daa5399e71e3ce8a5cf4434cf134f1
MD TAHER BIN OMAR HIJBULLAH <22303142@iubat.edu>
2026-07-08 09:03:48 +0000

    add Student model with fields for student information

--- scnd_prjct/config/st_info/models.py
+++ scnd_prjct/config/st_info/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    st_id = models.IntegerField("max_length = 20, unique = True")
+    st_name = models.CharField(max_length=100)
+    dept = models.CharField(max_length=100)
+    dob = models.DateField()
+    gender = models.CharField(max_length=10)
+    address = models.TextField()
+    def __str__(self):
+        return f"{self.st_id} - {self.st_name}"
+
